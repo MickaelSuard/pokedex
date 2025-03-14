a077d0287d02b36b01c820787d0e374ffbdbb1b1
Mickael Suard <suard.mickael72@gmail.com>
2025-03-14 18:51:36 +0100

    trad design system

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,14 @@
+import { useThemeColors } from "@/app/constants/hooks/useThemeColors";
+import { Shadows } from "@/app/constants/Shadows";
+import { useTheme } from "@react-navigation/native";
 import { View, ViewStyle, type ViewProps } from "react-native";
 
 type Props = ViewProps;
 
 
 export function Card ({style,...rest} : Props) {
- return <View style={[style,styles]} {...rest}>
+    const colors = useThemeColors();
+ return <View style={[style,styles,{backgroundColor : colors.grayWhite}]} {...rest}>
 
  </View>
 }
@@ -12,4 +16,5 @@ export function Card ({style,...rest} : Props) {
 const styles = {
     backgroundColor : '#FFF',
     borderRadius: 8,
+    ...Shadows.dp2,
 } satisfies ViewStyle
